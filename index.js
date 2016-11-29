c98ab97ab9a245ee2d872df0a52d5bd8b816178d
dietrich ayala <autonome@gmail.com>
2016-11-28 22:30:52 -0800

    moar web extension

--- index.js
+++ index.js
@@ -1,43 +1,11 @@
 var activeTab = null;
 
-browser.tabs.onActivated.addListener(function(tab) {
-  console.log('active tab', tab)
-  activeTab = tab;
+browser.tabs.onActivated.addListener(function(activeInfo) {
+  browser.tabs.get(activeInfo.tabId).then(function(tabInfo) {
+    activeTab = tabInfo;
+  });
 });
 
 browser.tabs.onCreated.addListener(function(newTab) {
-  var opts = {
-    currentWindow: true,
-    active: true
-  };
-  console.log('new tab index', newTab.index);
-  //browser.tabs.query(opts, function(result) {
-    //var activeTab = result[0];
-    console.log("new tab! changing newTab index (", newTab.index, ") to active tab index (", activeTab.index, ") + 1");
-    newTab.index = activeTab.index + 1;
-  //});
+  browser.tabs.move(newTab.id, { index: activeTab.index + 1 });
 });
-
-
-/*
-var windows = require("sdk/windows").browserWindows;
-
-function onWindowOpen(window) {
-  var lastActiveTabIndex = window.tabs.activeTab.index;
-
-  window.tabs.on('activate', function onActivate(tab) {
-    lastActiveTabIndex = tab.index;
-  });
-
-  window.tabs.on('open', function onOpen(tab) {
-    var targetIndex = lastActiveTabIndex + 1;
-    if (tab.index != targetIndex)
-      tab.index = targetIndex;
-  });
-}
-
-for each (var window in windows)
-  onWindowOpen(window);
-
-windows.on('open', onWindowOpen);
-*/
