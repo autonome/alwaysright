d8d9b57a61fad4300f48079080efe0a7e495a85d
dietrich ayala <autonome@gmail.com>
2016-11-28 22:06:23 -0800

    web extension conversion

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
+var activeTab = null;
+
+browser.tabs.onActivated.addListener(function(tab) {
+  console.log('active tab', tab)
+  activeTab = tab;
+});
+
 browser.tabs.onCreated.addListener(function(newTab) {
   var opts = {
     currentWindow: true,
     active: true
   };
-  browser.tabs.query(opts, function(result) {
-    var activeTab = result[0];
+  console.log('new tab index', newTab.index);
+  //browser.tabs.query(opts, function(result) {
+    //var activeTab = result[0];
+    console.log("new tab! changing newTab index (", newTab.index, ") to active tab index (", activeTab.index, ") + 1");
     newTab.index = activeTab.index + 1;
-  });
+  //});
 });
 
 
@@ -27,7 +36,7 @@ function onWindowOpen(window) {
   });
 }
 
-for (let window of windows)
+for each (var window in windows)
   onWindowOpen(window);
 
 windows.on('open', onWindowOpen);
