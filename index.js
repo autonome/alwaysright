c6a9c235ef444c30643b1e205ad8775a63cb489f
dietrich ayala <autonome@gmail.com>
2017-05-09 15:01:26 +0800

    fix no default active tab, rev version number, set addon id

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 var activeTab = null;
 
+// Get active tab reference on load.
+browser.tabs.query({currentWindow: true, active: true}).then(function(tabs) {
+  browser.tabs.get(tabs[0].id).then(function(tabInfo) {
+    activeTab = tabInfo;
+  });
+});
+
+// Update reference to active tab any tab there's a tab
+// activated event.
 browser.tabs.onActivated.addListener(function(activeInfo) {
   browser.tabs.get(activeInfo.tabId).then(function(tabInfo) {
     activeTab = tabInfo;
   });
 });
 
+// Any time a new tab is created, set it's index to the index
+// of the active tab, plus one.
 browser.tabs.onCreated.addListener(function(newTab) {
   browser.tabs.move(newTab.id, { index: activeTab.index + 1 });
 });
