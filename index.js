cb0094d92d60aab2f403a51997c57bb16cf12fe5
dietrich ayala <autonome@gmail.com>
2017-08-08 18:48:43 +0800

    fix #9, more webextension migration work, and release

--- index.js
+++ index.js
@@ -1,25 +1,95 @@
+// Reference to the active tab
 var activeTab = null;
 
-// Get active tab reference on load.
-browser.tabs.query({currentWindow: true, active: true}).then(function(tabs) {
-  browser.tabs.get(tabs[0].id).then(function(tabInfo) {
-    activeTab = tabInfo;
+// Update cached ref to active tab
+function updateActiveTab() {
+  browser.tabs.query({currentWindow: true, active: true}).then(function(tabs) {
+    browser.tabs.get(tabs[0].id).then(function(tab) {
+      if (!activeTab) {
+        initEventHandlers(tab);
+      }
+      activeTab = tab;
+    });
   });
-});
+}
 
-// Update reference to active tab any tab there's a tab
-// activated event.
-browser.tabs.onActivated.addListener(function(activeInfo) {
-  browser.tabs.get(activeInfo.tabId).then(function(tabInfo) {
-    activeTab = tabInfo;
+// Get active tab reference on startup
+updateActiveTab();
+
+// As getting active tab id at startup is async, really start our own
+// activity until we actually have it.
+// This is called in updateActiveTab if activeTab is null.
+function initEventHandlers() {
+  // Update reference to active tab any time there's a tab
+  // activated event.
+  browser.tabs.onActivated.addListener(function(activeInfo) {
+    browser.tabs.get(activeInfo.tabId).then(function(tabInfo) {
+      activeTab = tabInfo;
+    });
   });
-});
 
-// Any time a new tab is created, set it's index to the index
-// of the active tab, plus one.
-browser.tabs.onCreated.addListener(function(newTab) {
-  var targetIndex = activeTab.index + 1;
-  if (newTab.index != targetIndex) {
-    browser.tabs.move(newTab.id, { index: activeTab.index + 1 });
+  // Any time a new tab is created, set its index to the index
+  // of the active tab, plus one.
+  browser.tabs.onCreated.addListener(makeRight);
+
+  // ODD. If instead of this, I get a fresh reference to the active tab
+  // right before moving, it still has stale index!!
+  // Soooo, I guess we're doing this.
+  browser.tabs.onMoved.addListener(updateActiveTab);
+}
+
+// Move the referenced tab to the immediate right of the active tab,
+// or to the immediate right of the last pinned tab.
+function makeRight(newTab) {
+  // Too soon after startup.
+  if (!activeTab) {
+    return;
   }
-});
+
+  // The new tab either dragged to new window or something went wrong.
+  if (newTab.windowId != activeTab.windowId) {
+    return;
+  }
+
+  var targetIndex = activeTab.index + 1;
+
+  // Only bother moving if it wouldn't organically be placed immediately to the
+  // right of the active tab.
+  if (newTab.index == targetIndex) {
+    return;
+  }
+
+  // We need current window for a few things required for correct tab placement.
+  // And apparently tab references go STALE. Dammit.
+  browser.windows.getCurrent({populate: true}).then(function(win) {
+
+    // Maybe is a restored tab, or another add-on, or something else is wonky.
+    if (newTab.index < win.tabs.length - 1
+        || newTab.index > win.tabs.length - 1) {
+      return;
+    }
+
+    // If the active tab is pinned, we have to set the target index
+    // to that of the first non-pinned tab.
+    if (activeTab.pinned) {
+      targetIndex = getFirstNonPinnedTab(win).index;
+    }
+
+    // YOU GOT TO MOVE IT MOVE IT
+    browser.tabs.move(newTab.id, { index: targetIndex }).then(function(t) {
+      // woohoo.
+    }, function(e) {
+      console.log('AlwaysRight: tab move fail', e);
+    });
+  });
+}
+
+// Return a tab object for the first non-pinned tab in the tab strip
+// for the given window.
+function getFirstNonPinnedTab(win) {
+  for (var tab of win.tabs) {
+    if (!tab.pinned) {
+      return tab;
+    }
+  }
+}
